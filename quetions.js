8ec1bf99695c1dc1fed6151089c1d0c9a8ec9808
Dhara Solanki <83348790+Dhara3078@users.noreply.github.com>
2022-01-25 17:22:08 +0530

    Added Files

---
+++ quetions.js
@@ -0,0 +1,59 @@
+//creating an array and passing the number , question, and answer
+let questions = [
+    {
+        num: 1,
+        question: "'OS' computer abbreviation usually means?",
+        answer: "Operating System",
+        options: [
+            "Order of Significance",
+            "Open Software",
+            "Operating System",
+            "Optical Sensor"
+        ]
+    },
+    {
+        num: 2,
+        question: "A page designed in HTML is called _____",
+        answer: "Web Page",
+        options: [
+            "Application",
+            "Cover page",
+            "Front-end",
+            "Web Page"
+        ]
+    },
+    {
+        num: 3,
+        question: "An HTML document is saved with the ____ extension.",
+        answer: " .html",
+        options: [
+            " .htl",
+            " .html",
+            " .hml",
+            " .htnl"
+        ]
+    },
+    {
+        num: 4,
+        question: "The HTML document contains a root tag called ____",
+        answer: "HTML",
+        options: [
+            "HEAD",
+            "Title",
+            "Body",
+            "HTML"
+        ]
+    },
+    {
+        num: 5,
+        question: "If we want to place text around an image, which CSS property should we use?",
+        answer: "float",
+        options: [
+            "push",
+            "float",
+            "align",
+            "wrap"
+        ]
+    },
+
+];
